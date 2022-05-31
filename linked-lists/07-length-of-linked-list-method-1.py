f2855073a30192418b50a2ce687c198e2d84bc52
hashbanger <prashant.brahmbhatt32@outlook.com>
2022-05-31 19:47:16 +0530

    length of the list

---
+++ linked-lists/07-length-of-linked-list-method-1.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # Function to insert a new node at the beginning
+    def insert_at_front(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    # Finding the length of the list
+    def get_length(self):
+        list_length = 0
+        current = self.head
+        # iterate until the last node
+        while current is not None:
+            current = current.next
+            list_length += 1
+        return list_length
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end=" ")
+            current = current.next
+
+if __name__ == '__main__':
+    llist = LinkedList()
+    llist.insert_at_front(4)
+    llist.insert_at_front(3)
+    llist.insert_at_front(6)
+    llist.insert_at_front(9)
+    llist.insert_at_front(2)
+    llist.insert_at_front(1)
+    llist.print_list()
+    print(f"\nLength of the List is {llist.get_length()}")
---
+++ linked-lists/08-length-of-linked-list-method-2.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # Function to insert a new node at the beginning
+    def insert_at_front(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    # Finding the length of the list recursively
+    def get_length(self, node):
+        if node is None:
+            return 0
+        else:
+            return 1 + self.get_length(node.next)
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end=" ")
+            current = current.next
+
+if __name__ == '__main__':
+    llist = LinkedList()
+    llist.insert_at_front(4)
+    llist.insert_at_front(3)
+    llist.insert_at_front(6)
+    llist.insert_at_front(9)
+    llist.insert_at_front(7)
+    llist.insert_at_front(2)
+    llist.insert_at_front(1)
+    llist.print_list()
+    print(f"\nLength of the List is {llist.get_length(llist.head)}")
