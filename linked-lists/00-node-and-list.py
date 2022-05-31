255bdcd9b9c84c52d7bd035556ac057bd530d931
hashbanger <prashant.brahmbhatt32@outlook.com>
2022-05-31 19:46:36 +0530

    simple implementation

---
+++ linked-lists/00-node-and-list.py
@@ -0,0 +1,12 @@
+# The Node Class
+class Node:
+    # a function to initialize the node object
+    def __init__(self, data):
+        self.data = data # the assigned data
+        self.next = None # the next node
+
+# The Linked List Class
+class LinkedList:
+    # Function to initialize the Linked List
+    def __init__(self):
+        self.head = None
---
+++ linked-lists/01-simple-linked-list.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+if __name__ == '__main__':
+    # created an empty list
+    llist = LinkedList()
+
+    firstNode = Node(1)
+    secondNode = Node(2)
+    thirdNode = Node(3)
+
+    # assigning the head to first node
+    llist.head = firstNode
+    # connecting second to the first
+    llist.head.next = secondNode
+    # connecting third node to the second
+    secondNode.next = thirdNode
+
+    print(llist.head.data)
+    print(llist.head.next.data)
+    print(llist.head.next.next.data)
+
