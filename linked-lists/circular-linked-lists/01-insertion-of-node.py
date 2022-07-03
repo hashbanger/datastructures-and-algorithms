b22545db2b4fd92968906b9686f444be8375af48
hashbanger <prashant.brahmbhatt32@outlook.com>
2022-07-03 14:52:12 +0530

    updated insertion and traversal with head and tail

--- linked-lists/circular-linked-lists/01-insertion-of-node.py
+++ linked-lists/circular-linked-lists/01-insertion-of-node.py
@@ -5,100 +5,95 @@ class Node:
 
 class LinkedList:
     def __init__(self):
-        self.last = None
+        self.head = None
+        self.tail = None
 
-    # Function to insert a new node at the beginning
     def insert_in_empty(self, new_data):
-        # if not empty return the last
-        if self.last != None:
-            return self.last
-
+        # if not empty return the head
+        if self.head != None:
+            return self.head
+        
         # creating a new node
         new_node = Node(new_data)
-        self.last = new_node
 
-        # creating connections
-        self.last.next = self.last
-        return self.last
+        # creating connection
+        self.head = new_node
+        self.tail = new_node
+        new_node.next = new_node
+
+        return self.head
     
     def insert_at_front(self, new_data):
-        # if None do empty insertion
-        if self.last == None:
+        if self.head == None:
             return self.insert_in_empty(new_data)
-        
+
         # creating and connecting
         new_node = Node(new_data)
-        new_node.next = self.last.next
-        self.last.next = new_node
-
-        return self.last
+        new_node.next = self.head
+        self.head = new_node
+        self.tail.next = new_node
 
     def insert_at_end(self, new_data):
-        # if None do empty insertion
-        if self.last == None:
-            return self.insert_in_empty(new_data)
-        
+        if self.head == None:
+            return serlf.insert_in_empty(new_data)
+
         # creating and connecting
         new_node = Node(new_data)
-        new_node.next = self.last.next
-        self.last.next = new_node
-        self.last = new_node
+        new_node.next = self.head
+        self.tail.next = new_node
+        self.tail = new_node
 
     def insert_after(self, previous_node, new_data):
         # if None return None
-        if self.last == None:
+        if self.head == None:
             return None
-
-        # start a pointer from the head
-        ptr = self.last.next
-        # keep traversing
+        
+        ptr = self.head
         while ptr != None:
-
             # if node found, change connections
             if ptr.data == previous_node:
                 new_node = Node(new_data)
                 new_node.next = ptr.next
                 ptr.next = new_node
 
-                # if the node is also last node 
+                # if the node is also the last node
                 # make new node the last node
-                if ptr == self.last:
-                    self.last = new_node
-                    return self.last
+                if ptr == self.tail:
+                    self.tail = new_node
+                    return
                 else:
-                    return self.last
+                    return
+
+            # if it circles back, node not found
+            if ptr.next == self.head:
+                print(previous_node, " not present in the list")
+                break
 
             ptr = ptr.next
 
-            # if it circles back, node not found
-            if ptr == self.last.next:
-                print(previous_node, " not present in teh list")
-                break
-
-        new_node = Node(new_data)
-
     def print_list(self):
-        head = self.last.next
+        ptr = self.head
         while True:
-            print(head.data, end=" - ")
-            head = head.next
-            if head == self.last.next:
-                print(head.data)
+            print(ptr.data, end = ' - ')
+            if ptr.next == self.head:
                 break
+            ptr = ptr.next
 
 if __name__ == '__main__':
     llist = LinkedList()
     # inserting 5 as the head
     llist.insert_in_empty(5)
-    # inserting 3 before 5
+    # # inserting 3 before 5
     llist.insert_at_front(3)
     # # inserting 2 before 3
     llist.insert_at_front(2)
     # inserting 6 at the end
-    llist.insert_at_end(7)
+    llist.insert_at_end(6)
     # inserting 4 after 3
     llist.insert_after(3, 4)
-    # inserting 6 after 5
-    llist.insert_after(5, 6)
-    # # traversing the list
+    # inserting 7 after 6
+    llist.insert_after(6, 7)
+    # trying to insert 9 after a non-exiting node
+    llist.insert_after(8, 9)
+    # traversing the list
     llist.print_list()
