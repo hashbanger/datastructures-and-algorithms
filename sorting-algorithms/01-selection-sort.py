fcb28a50119fe87ccca238b0a857cb7f0c50bf57
hashbanger <prashant.brahmbhatt32@outlook.com>
2022-07-04 16:05:08 +0530

    added selection sort

---
+++ sorting-algorithms/01-selection-sort.py
@@ -0,0 +1,24 @@
+# Selection sort
+# Time Complexity: O(N^2)
+# Auxilary Space: O(1) only extra temporary variable is used for swapping
+def selection_sort(array):
+
+    for i in range(len(array)):
+
+        # take the current element as the minimum
+        min_idx = i
+        
+        # look for the minimum number in the remaining array
+        for j in range(i+1, len(array)):
+            if array[min_idx] > array[j]:
+                min_idx = j
+
+        # swap places with the minimum in the remaining array
+        array[min_idx], array[i] = array[i], array[min_idx]
+
+    return array
+
+if __name__ == '__main__':
+    input_array = list(map(int, input().split()))
+    print("Input Array: ", input_array)
+    print("Sorted Array: ", selection_sort(input_array))
