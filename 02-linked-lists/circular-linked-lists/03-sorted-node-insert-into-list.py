c652656bb5a792f84868832b0784a8c563313f6d
hashbanger <prashant.brahmbhatt32@outlook.com>
2022-09-30 15:33:24 +0530

    changed folder names

--- 02-linked-lists/circular-linked-lists/03-sorted-node-insert-into-list.py
+++ 02-linked-lists/circular-linked-lists/03-sorted-node-insert-into-list.py
@@ -37,9 +37,11 @@ class LinkedList:
     def sorted_insert(self, new_data):
         new_node = Node(new_data)
         ptr = self.head
+
         # case 1, if the list is empty
         if self.head is None:
             self.insert_in_empty(new_data)
+
         # case 2, if the value is less than head
         elif new_node.data <= ptr.data:
             while ptr.next != self.head:
@@ -47,6 +49,7 @@ class LinkedList:
             new_node.next = self.head
             ptr.next = new_node
             self.head = new_node
+
         # case 3, locating the node before insert point
         else:
             while (ptr.next != self.head) and (ptr.next.data < new_node.data):
@@ -70,9 +73,12 @@ class LinkedList:
 
 if __name__ == "__main__":
     input_values = [12, 32, 44, 23, 5, 0, 11]
+
     print("Values to insert")
     print(input_values)
+
     llist = LinkedList()
+
     print("\nSorted Insert")
     for value in input_values:
         llist.sorted_insert(value)
