18783164c7e19ab8477c908cc59bf8b3c1446af3
hashbanger <prashant.brahmbhatt32@outlook.com>
2022-10-01 12:07:26 +0530

    reversing a string using stack

---
+++ 03-stacks/03-reverse-a-string-using-stack.py
@@ -0,0 +1,59 @@
+# reversing a string using stack
+class Stack:
+    def __init__(self, size):
+        self.top = -1
+        self.size = size
+        self.elements = []
+
+    def is_empty(self):
+        return True if self.top == -1 else False
+
+    def push(self, item):
+        if self.top < self.size - 1:
+
+            # adding the element to the stack
+            self.elements.append(item)
+
+            # increasing the top to one position up
+            self.top += 1
+        else:
+            print("Can't push. Stack overflow!")
+
+    def pop(self):
+        if self.top != -1:
+
+            # popping the last element from the stack
+            item = self.elements.pop()
+
+            # decreasing the top to one position down
+            self.top -= 1
+            return item
+        else:
+            print("Can't pop. Stack underflow!")
+
+    def peek(self):
+        return self.elements[self.top]
+
+    def reverse_string(self, input_string):
+
+        # first we push all the elements to the stack
+        for ch in input_string:
+            self.push(ch)
+
+        # empty the string, we would use it to store the results
+        input_string = ""
+
+        # popping the elements are adding them back to the string
+        while self.top != -1:
+            input_string += self.pop()
+
+        return input_string
+
+
+if __name__ == "__main__":
+    input_string = "cogito ergo sum"
+    print(f"Input String: {input_string}")
+
+    stack = Stack(size=len(input_string))
+    reverse_string = stack.reverse_string(input_string)
+    print(f"Reverse String: {reverse_string}")
