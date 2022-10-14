9e5b858cab2d01364505b852b68752fc38249262
hashbanger <prashant.brahmbhatt32@outlook.com>
2022-10-14 19:33:21 +0530

    added implementation of queue

---
+++ 04-queues/00-implementation-of-queue.py
@@ -0,0 +1,76 @@
+# Implementing a queue from scratch along with the common operations
+class Queue:
+    def __init__(self, size):
+        self.size = size
+        self.front = -1
+        self.rear = -1
+        self.count = 0
+        self.elements = [None] * self.size
+
+    def get_size(self):
+        return self.count
+
+    def is_empty(self):
+        return self.get_size() == 0
+
+    def is_full(self):
+        return self.get_size() == self.size
+
+    def enqueue(self, item):
+        if self.is_full():
+            print(f"Queue Overflow by {item}")
+            return
+        print(f"enqueued {item}")
+
+        # if its is the first item then we initialize the front to the first spot
+        if self.front == -1:
+            self.front = 0
+
+        # increase the rear by one position (make it capable to circle back to empty places)
+        self.rear = (self.rear + 1) % self.size
+
+        # then insert the item at the new rear position
+        self.elements[self.rear] = item
+        self.count += 1
+
+    def dequeue(self):
+        if self.is_empty():
+            print(f"Queue Underflow!")
+            return
+
+        # get the front element
+        item = self.elements[self.front]
+        print(f"dequeued {item}")
+
+        # increase the front to the next inserted element (make it capable to circle back)
+        self.front = (self.front + 1) % self.size
+        self.count -= 1
+        return item
+
+    def peek(self):
+        if not self.is_empty():
+            print(f"front element {self.elements[self.front]}")
+            return self.elements[self.front]
+
+        print("Queue Underflow!")
+        return
+
+
+if __name__ == "__main__":
+    queue = Queue(5)
+    queue.enqueue(2)
+    queue.enqueue(4)
+    queue.enqueue(6)
+    queue.enqueue(8)
+    queue.enqueue(10)
+    queue.enqueue(12)
+
+    queue.dequeue()
+    queue.dequeue()
+
+    queue.peek()
+
+    queue.enqueue(12)
+    queue.enqueue(14)
+
+    queue.peek()
