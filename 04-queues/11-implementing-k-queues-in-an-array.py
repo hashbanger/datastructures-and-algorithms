a7ae790dd2948339687b199a1c80d6df3833e290
hashbanger <prashant.brahmbhatt32@outlook.com>
2022-10-21 16:57:07 +0530

    implemented k queues in an array

---
+++ 04-queues/11-implementing-k-queues-in-an-array.py
@@ -0,0 +1,80 @@
+# implementing k-number of queues in a single array with space efficient approach
+
+
+class KQueues:
+    def __init__(self, n_queues, size):
+        self.size = size
+        self.elements = [None] * self.size
+        self.fronts = [-1] * n_queues
+        self.rears = [-1] * n_queues
+        self.nexts = list(range(1, self.size))
+        self.nexts.append(-1)
+
+        self.free = 0
+
+    def is_empty(self, q_num):
+        return self.fronts[q_num] == -1
+
+    def is_full(self, q_num):
+        return self.free == -1
+
+    def enqueue(self, q_num, item):
+        if self.is_full(q_num):
+            print(f"Queue {q_num} Overflow by {item}")
+            return
+
+        # get the next free slot index
+        next_free = self.nexts[self.free]
+        if self.fronts[q_num] == -1:
+            self.fronts[q_num] = self.rears[q_num] = self.free
+        else:
+            # connect the previous element to the new element spot
+            self.nexts[self.rears[q_num]] = self.free
+
+            # update the rear to current spot
+            self.rears[q_num] = self.free
+
+        # push the item
+        self.elements[self.free] = item
+
+        # to indicate the queue ends at that place
+        self.nexts[self.rears[q_num]] = -1
+
+        # update the free spot
+        self.free = next_free
+
+    def dequeue(self, q_num):
+        if self.is_empty(q_num):
+            print(f"Queue {q_num} Underflow!")
+            return
+
+        front_idx = self.fronts[q_num]
+        item = self.elements[front_idx]
+
+        # update the front to next element
+        self.fronts[q_num] = self.nexts[front_idx]
+
+        # update the next of current front to free spot
+        self.nexts[front_idx] = self.free
+
+        # make current front as new free spot
+        self.free = front_idx
+        return item
+
+
+if __name__ == "__main__":
+    kqueue = KQueues(3, 8)
+    kqueue.enqueue(0, 1)
+    kqueue.enqueue(0, 2)
+    kqueue.enqueue(0, 3)
+    kqueue.enqueue(1, 4)
+    kqueue.enqueue(1, 5)
+    kqueue.enqueue(0, 6)
+    kqueue.enqueue(2, 7)
+    kqueue.enqueue(2, 8)
+    kqueue.dequeue(0)
+    kqueue.dequeue(0)
+    kqueue.enqueue(0, 9)
+    kqueue.enqueue(0, 10)
+    kqueue.dequeue(0)
+    kqueue.dequeue(0)
