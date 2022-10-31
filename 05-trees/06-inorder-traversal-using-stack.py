7874e04128fc90ee0d0767677bafaf41c3481b7f
hashbanger <prashant.brahmbhatt32@outlook.com>
2022-10-31 10:59:39 +0530

    renamed file

---
+++ 05-trees/06-inorder-traversal-using-stack.py
@@ -0,0 +1,136 @@
+# implementing inorder traversal of a binary tree using a stack rather than recursion.
+class Stack:
+    def __init__(self, size):
+        self.top = -1
+        self.size = size
+        self.elements = []
+
+    def is_empty(self):
+        return self.top == -1
+
+    def is_full(self):
+        return self.top == (self.size - 1)
+
+    def push(self, item):
+        if not self.is_full():
+
+            # adding the element to the stack
+            self.elements.append(item)
+
+            # increasing the top to one position up
+            self.top += 1
+        else:
+            return False
+
+    def pop(self):
+        if not self.is_empty():
+
+            # popping the last element from the stack
+            item = self.elements.pop()
+
+            # decreasing the top to one position down
+            self.top -= 1
+            return item
+        else:
+            return False
+
+    def peek(self):
+        if not self.is_empty():
+            return self.elements[self.top]
+        print("stack underflow!")
+        return
+
+
+class BinaryTree:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def insertion(self, new_data):
+        if self.data:
+
+            # if the new node is smaller then go left
+            if new_data < self.data:
+
+                # if the left is empty then insert
+                if self.left is None:
+                    self.left = BinaryTree(new_data)
+
+                # otherwise recursively go to the left till empty spot is found
+                else:
+                    self.left.insertion(new_data)
+
+            # if the node is greater then go right
+            elif new_data > self.data:
+
+                # if the right is empty then insert
+                if self.right is None:
+                    self.right = BinaryTree(new_data)
+
+                # otherwise recursively go to the right till empty spot is found
+                else:
+                    self.right.insertion(new_data)
+
+            # raise error otherwise for duplicates
+            else:
+                print(f"Node {new_data} already exists.")
+                return
+        else:
+            self.data = new_data
+
+
+def inorder_traversal(tree):
+    if tree is None:
+        return
+
+    # recursively visit the left child until its None
+    inorder_traversal(tree.left)
+
+    # output the root value
+    print(tree.data, end=" ")
+
+    # recursively visit the right child until its None
+    inorder_traversal(tree.right)
+
+
+def inorder_traversal_using_stack(tree):
+    stack = Stack(50)
+
+    current = tree
+    while True:
+        if current:
+            stack.push(current)
+            current = current.left
+
+        elif not stack.is_empty():
+            item = stack.pop()
+            print(item.data, end=" ")
+            current = item.right
+        else:
+            break
+
+
+if __name__ == "__main__":
+    btree = BinaryTree(25)
+    btree.insertion(15)
+    btree.insertion(50)
+    btree.insertion(10)
+    btree.insertion(22)
+    btree.insertion(35)
+    btree.insertion(70)
+    btree.insertion(4)
+    btree.insertion(12)
+    btree.insertion(18)
+    btree.insertion(24)
+    btree.insertion(31)
+    btree.insertion(44)
+    btree.insertion(66)
+    btree.insertion(90)
+    btree.insertion(2)
+
+    print("\nInorder Traversal with recursion:")
+    inorder_traversal(btree)
+
+    print("\n\nInorder Traversal using stack:")
+    inorder_traversal_using_stack(btree)
