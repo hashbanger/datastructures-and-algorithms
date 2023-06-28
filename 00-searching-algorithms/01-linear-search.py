fe89ab4d8a4cf75b6b2e661faaeaf3993fe4c559
hashbanger <prashant.brahmbhatt32@outlook.com>
2023-06-28 17:02:26 +0530

    improved the class

--- 00-searching-algorithms/01-linear-search.py
+++ 00-searching-algorithms/01-linear-search.py
@@ -1,12 +1,15 @@
 # Linear Search
 
 class LinearSearch:
+    def __init__(self, array, num):
+        self.array = sorted(array)
+        self.num = num
     # Best-case: O(1)
     # Worst-case: O(n)
     # Average-case: O(n)
-    def linear_search(self, array, num):
-        for i in range(len(array)):
-            if array[i] == num:
+    def linear_search(self, array=None, num=None):
+        for i in range(len(self.array)):
+            if self.array[i] == self.num:
                 return i
         return -1
 
@@ -15,19 +18,19 @@ class LinearSearch:
     # Best-case: O(1)
     # Worst-case: O(n)
     # Average-case: O(n)
-    def linear_search_two_pointers(self, array, num):
+    def linear_search_two_pointers(self, array=None, num=None):
         left = 0
-        right = len(array) - 1
+        right = len(self.array) - 1
 
         # moving from the left and right
         while left <= right:
 
             # if found from the left side
-            if array[left] == num:
+            if self.array[left] == self.num:
                 return left
 
             # if found from right side
-            if array[right] == num:
+            if self.array[right] == self.num:
                 return right
 
             left = left + 1
@@ -39,12 +42,12 @@ class LinearSearch:
     # Best-case: O(1)
     # Worst-case: O(n)
     # Average-case: O(n)
-    def linear_search_sorted_array(self, array, num):
-        sorted_array = sorted(array)
+    def linear_search_sorted_array(self, array=None, num=None):
+        sorted_array = sorted(self.array)
         for i in range(len(sorted_array)):
-            if sorted_array[i] == num:
+            if sorted_array[i] == self.num:
                 return True
-            if sorted_array[i] > num:
+            if sorted_array[i] > self.num:
                 return False
         return False
 
@@ -56,21 +59,21 @@ if __name__ == "__main__":
 
     lsearch = LinearSearch(array, num)
     print("\nSimple Linear Search")
-    result = lsearch.linear_search(array, num)
+    result = lsearch.linear_search()
     if result != -1:
         print("Number found at position:", result + 1)
     else:
         print("Number not found!")
 
     print("\nLinear Search Using Two Pointers")
-    result = lsearch.linear_search_two_pointers(array, num)
+    result = lsearch.linear_search_two_pointers()
     if result != -1:
         print("Number found at position:", result + 1)
     else:
         print("Number not found!")
 
     print("\nLinear Search for Sorted Array")
-    result = lsearch.linear_search_sorted_array(array, num)
+    result = lsearch.linear_search_sorted_array()
     if result != -1:
         print("Number found at position:", result + 1)
     else:
