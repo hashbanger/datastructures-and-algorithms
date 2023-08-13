999f08a9eb97580b5dfdb57aa5557a2e68c160f2
hashbanger <prashant.brahmbhatt32@outlook.com>
2023-08-13 13:42:45 +0530

    added explanation

--- 03-stacks/09-reverse-a-stack-method2.py
+++ 03-stacks/09-reverse-a-stack-method2.py
@@ -1,5 +1,8 @@
 # We will reverse the elements of the stack using recursion
-
+# First take the elements one by one and hold them like in a hand using recursion stack,
+# then insert the last picked out element into the stack.
+# Then before putting the next in line element, pick up in hand the ones already pushed again using recursion stack
+# and insert the current in the bottom.
 
 class Stack:
     def __init__(self):
